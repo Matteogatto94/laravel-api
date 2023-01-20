dc51d5044c50770e6428c59fadedc709be9e42c2
Matteogatto94 <cosenza94@live.it>
2023-01-20 16:55:28 +0100

    add route to return project in json format or null

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -15,4 +15,24 @@ class ProjectController extends Controller
             'data' => Project::with(['category', 'technologies'])->orderByDesc('id')->get()
         ]);
     }
+
+    public function show($slug)
+    {
+        //dd($slug);
+
+        $project = Project::with('category', 'technologies')->where('slug', $slug)->first();
+        //dd($project);
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Project Not Found'
+            ]);
+        }
+    }
 }
