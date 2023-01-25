67a9fc55e37662126c16fe1f1f74f1d7acf4c5cc
Matteogatto94 <cosenza94@live.it>
2023-01-25 16:07:04 +0100

    fix error to see mail

--- app/Http/Controllers/API/LeadController.php
+++ app/Http/Controllers/API/LeadController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Lead;
+use App\Mail\NewContact;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
@@ -15,5 +17,7 @@ class LeadController extends Controller
         $new_lead = new Lead();
         $new_lead->fill($data);
         $new_lead->save();
+
+        Mail::to('admin@laravel.it')->send(new NewContact($new_lead));
     }
 }
--- resources/views/mails/new-contact-mail.blade.php
+++ resources/views/mails/new-contact-mail.blade.php
@@ -1,10 +1,10 @@
 <h1>Ciao Admin!</h1>
 
 <p>
-    Hai un nuovo messaggio:<br>
+    Hai un nuovo messaggio<br>
     Ecco i dettagli: <br>
-    Nome: {{$lead->name}}
-    Email: {{$lead->email}}
+    Nome: {{$lead->name}}<br>
+    Email: {{$lead->email}}<br>
     Messaggio: <br>
     {{$lead->message}}
 </p>
